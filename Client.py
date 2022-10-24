41bacb63799d930e8206122ec93e875fdf502a02
samman375 <samuel.thorley2000@gmail.com>
2022-10-24 17:36:04 +1100

    Finish authentication

--- Client.py
+++ Client.py
@@ -1,15 +1,18 @@
 """
-Usage: python3 Client3.py localhost 12000
+Usage: python3 client.py localhost 12000
+
+Adapted from example multi-threaded client code on course homepage
 """
 from socket import *
 import sys
 
 # Server would be running on the same host as Client
-if len(sys.argv) != 3:
-    print("\n===== Error usage, python3 Client3.py SERVER_IP SERVER_PORT ======\n")
+if len(sys.argv) != 4:
+    print("\n===== Error usage, python3 client.py SERVER_IP SERVER_PORT CLIENT_UDP_SERVER_PORT ======\n")
     exit(0)
 serverHost = sys.argv[1]
 serverPort = int(sys.argv[2])
+clientUDPServerPort = int(sys.argv[3])
 serverAddress = (serverHost, serverPort)
 
 # define a socket for the client side, it would be used to communicate with the server
@@ -19,8 +22,8 @@ clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect(serverAddress)
 
 while True:
-    message = input("===== Please type any messsage you want to send to server: =====\n")
-    clientSocket.sendall(message.encode())
+    # message = input("===== Please type any messsage you want to send to server: =====\n")
+    # clientSocket.sendall(message.encode())
 
     # receive response from the server
     # 1024 is a suggested packet size, you can specify it as 2048 or others
@@ -30,17 +33,52 @@ while True:
     # parse the message received from server and take corresponding actions
     if receivedMessage == "":
         print("[recv] Message from server is empty!")
-    elif receivedMessage == "user credentials request":
-        print("[recv] You need to provide username and password to login")
+    
+
+    ### Auth related:
+    
+    # Get and send username
+    elif receivedMessage == "username authentication request" or receivedMessage == "retry username authentication request":
+        if receivedMessage == "retry username authentication request":
+            print("Invalid Username. Please try again.")
+        message = input("Username: ").strip()
+        clientSocket.send(message.encode())
+    # Get and send password
+    elif receivedMessage == "password authentication request" or receivedMessage == "retry password authentication request":
+        if receivedMessage == "retry password authentication request":
+            print("Invalid Password. Please try again.")
+        message = input("Password: ").strip()
+        clientSocket.send(message.encode())
+    # Max failed auth attempts. Account blocked.
+    elif receivedMessage == "max failed attempts":
+        print("Invalid Password. Your account has been blocked for 10s. Please try again later")
+        break
+    # Attempt to login to blocked account
+    elif receivedMessage == "blocked account":
+        print("Your account is blocked due to multiple authentication failures. Please try again later")
+        break
+    
+    ### Commands:
+    
+    # Get command
+    elif receivedMessage == "welcome":
+        print(f"Welcome!")
+        message = input("Enter one of the following commands (EDG, UED, SCS, DTE, AED, OUT): ").strip()
+        clientSocket.send(message.encode())
+    
+    ### Misc:
+
     elif receivedMessage == "download filename":
         print("[recv] You need to provide the file name you want to download")
     else:
-        print("[recv] Message makes no sense")
-        
+        print("[recv] Error: Unknown server response received")
+    
     ans = input('\nDo you want to continue(y/n) :')
     if ans == 'y':
         continue
     else:
+        message = "exit"
+        clientSocket.send(message.encode())
         break
 
 # close the socket
--- Server.py
+++ Server.py
@@ -1,22 +1,77 @@
 """
-Usage: python3 server3.py localhost 12000
+    Usage: python3 server.py localhost 12000
+
+    Adapted from example multi-threaded server code on course homepage
 """
 from socket import *
 from threading import Thread
-import sys, select
+import sys, select, time
 
-# Acquire server port and num fail attempts from command line parameter
+# Acquire server port and max fail attempts from command line parameter
 if len(sys.argv) != 3:
-    print("\n===== Error usage, python3 Server3.py SERVER_PORT, NUM_CONSECUTIVE_FAIL_ATTEMPTS ======\n")
+    print("\nError usage: python3 server.py SERVER_PORT, NUM_CONSECUTIVE_FAIL_ATTEMPTS")
     exit(0)
 serverHost = "127.0.0.1"
 serverPort = int(sys.argv[1])
-numFailAttempts = int(sys.argv[2])
+maxFailAttempts = int(sys.argv[2])
 serverAddress = (serverHost, serverPort)
+
+if maxFailAttempts < 1 or maxFailAttempts > 5:
+    print("\nError usage: NUM_CONSECUTIVE_FAIL_ATTEMPTS must be between 1 and 5 (inclusive)")
+    exit(0)
 
 # define socket for the server side and bind address
 serverSocket = socket(AF_INET, SOCK_STREAM)
 serverSocket.bind(serverAddress)
+
+"""
+    Data structs
+"""
+
+blockedAccounts = []
+
+
+"""
+    Helper functions
+"""
+
+# Given a usernamelooks for it in credentials file
+def usernameLookup(username):
+    credsFile = open("credentials.txt", "r")
+    credsLines = credsFile.readlines()
+    credsFile.close()
+    for line in credsLines:
+        lineUsername = line.split()[0]
+        if username == lineUsername:
+            return True
+    return False
+
+# Given a username and password looks for it in credentials file
+def passwordLookup(username, password):
+    credsFile = open("credentials.txt", "r")
+    credsLines = credsFile.readlines()
+    credsFile.close()
+    for line in credsLines:
+        lineCreds = line.split()
+        lineUsername = lineCreds[0]
+        linePassword = lineCreds[1]
+        if username == lineUsername:
+            if password == linePassword:
+                return True
+            else:
+                return False
+    return False
+
+def blockAccount(username):
+    blockedAccounts.append(username)
+    time.sleep(10)
+    blockedAccounts.remove(username)
+
+def checkBlocked(username):
+    if username in blockedAccounts:
+        return True
+    else:
+        return False
 
 """
     Define multi-thread class for client
@@ -32,6 +87,7 @@ class ClientThread(Thread):
         self.clientAddress = clientAddress
         self.clientSocket = clientSocket
         self.clientAlive = False
+        self.authenticated = False
         
         print("===== New connection created for: ", clientAddress)
         self.clientAlive = True
@@ -40,42 +96,126 @@ class ClientThread(Thread):
         message = ''
         
         while self.clientAlive:
+            
+            if not self.authenticated:
+                self.promptLogin()
+
+                # If still not authenticated user reached fail limit
+                if not self.authenticated:
+                    self.clientAlive = False
+                    print("===== user killed - ", clientAddress)
+                    break
+            
             # use recv() to receive message from the client
             data = self.clientSocket.recv(1024)
             message = data.decode()
             
             # if the message from client is empty, the client would be off-line then set the client as offline (alive=Flase)
-            if message == '':
+            if message == 'exit':
                 self.clientAlive = False
+                self.authenticated = False
                 print("===== the user disconnected - ", clientAddress)
                 break
             
             # handle message from the client
             if message == 'login':
-                print("[recv] New login request")
-                self.process_login()
+                print(f"[{clientAddress}:recv] New login request")
+                self.promptLogin()
             elif message == 'download':
-                print("[recv] Download request")
+                print(f"[{clientAddress}:recv] Download request")
                 message = 'download filename'
-                print("[send] " + message)
+                print(f"[{clientAddress}:send] " + message)
                 self.clientSocket.send(message.encode())
             else:
-                print("[recv] " + message)
-                print("[send] Cannot understand this message")
+                print(f"[{clientAddress}:recv] " + message)
+                print(f"[{clientAddress}:send] Cannot understand this message")
                 message = 'Cannot understand this message'
                 self.clientSocket.send(message.encode())
     
     """
         You can create more customized APIs here, e.g., logic for processing user authentication
-        Each api can be used to handle one specific function, for example:
-        def process_login(self):
-            message = 'user credentials request'
-            self.clientSocket.send(message.encode())
+        Each api can be used to handle one specific function
     """
-    def process_login(self):
-        message = 'user credentials request'
-        print('[send] ' + message);
+
+    # Authenticate User
+    def promptLogin(self):
+        failedAttempts = 0
+        validUsername = False
+        usernameClaim = ""
+
+        # Initial get username from client
+        message = 'username authentication request'
+        print(f'[{clientAddress}:send] ' + message)
         self.clientSocket.send(message.encode())
+
+        # Validate username
+        while not validUsername:
+            data = self.clientSocket.recv(1024)
+            usernameClaim = data.decode()
+            
+            if usernameLookup(usernameClaim):
+                if not checkBlocked(usernameClaim):
+                    # Successful authentication
+                    validUsername = True
+                else:
+                    # Valid credentials but account blocked
+                    message = "blocked account"
+                    print(f'[{clientAddress}:send] ' + message)
+                    self.clientSocket.send(message.encode())
+                    break
+            else:
+                failedAttempts += 1
+                if failedAttempts == maxFailAttempts:
+                    # Max failed attempts reached. Block account
+                    message = "max failed attempts"
+                    print(f'[{clientAddress}:send] ' + message)
+                    self.clientSocket.send(message.encode())
+                    blockAccount()
+                    break
+
+                # Re-request username
+                message = 'retry username authentication request'
+                print(f'[{clientAddress}:send] ' + message)
+                self.clientSocket.send(message.encode())
+
+        # Initial get password from client
+        message = 'password authentication request'
+        print(f'[{clientAddress}:send] ' + message)
+        self.clientSocket.send(message.encode())
+
+        # Validate password
+        while True:
+            data = self.clientSocket.recv(1024)
+            passwordClaim = data.decode()
+            
+            if passwordLookup(usernameClaim, passwordClaim):
+                if not checkBlocked(usernameClaim):
+                    # Successful authentication
+                    self.authenticated = True
+                    message = "welcome"
+                    print(f'[{clientAddress}:send] ' + message)
+                    self.clientSocket.send(message.encode())
+                    break
+                else:
+                    # Valid credentials but account blocked
+                    message = "blocked account"
+                    print(f'[{clientAddress}:send] ' + message)
+                    self.clientSocket.send(message.encode())
+                    break
+            else:
+                failedAttempts += 1
+                if failedAttempts == maxFailAttempts:
+                    # Max failed attempts reached. Block account
+                    message = "max failed attempts"
+                    print(f'[{clientAddress}:send] ' + message)
+                    self.clientSocket.send(message.encode())
+                    blockAccount()
+                    break
+
+                # Re-request credentials
+                message = 'retry password authentication request'
+                print(f'[{clientAddress}:send] ' + message)
+                self.clientSocket.send(message.encode())
 
 
 print("\n===== Server is running =====")
